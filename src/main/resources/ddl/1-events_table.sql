51c2f7d2dc4b8eb2fb2d0297cc9e78660cc928bd
Rodolfo <rodolfo.depaula@uberall.com>
2024-12-05 14:15:51 -0300

    new indexes

--- src/main/resources/ddl/1-events_table.sql
+++ src/main/resources/ddl/1-events_table.sql
@@ -1,5 +1,6 @@
 \connect postgres ;
 
+-- TODO
 -- https://dev.to/aws-heroes/scalable-sequence-for-postgresql-34o7
 -- https://theburningmonk.com/2024/11/eventbridge-best-practice-why-you-should-wrap-events-in-event-envelopes/
 
@@ -15,6 +16,8 @@ CREATE TABLE events
     CONSTRAINT domain_ids_length_check CHECK (array_length(domain_ids, 1) BETWEEN 1 AND 7)
 );
 
-CREATE INDEX domain_ids_gin_index
-    ON events USING gin (domain_ids);
+CREATE INDEX domain_ids_gin_index ON events USING gin (domain_ids);
 
+CREATE INDEX idx_events_event_type ON events(event_type);
+
+CREATE INDEX idx_events_correlation_id ON events(correlation_id);
