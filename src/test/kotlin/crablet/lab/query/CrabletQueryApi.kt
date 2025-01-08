d8734a20a499b55bf8077ca10ac3148fe1e26be4
Rodolfo <rodolfo.depaula@uberall.com>
2025-01-08 18:29:20 -0300

    moving query package to test scope

--- src/test/kotlin/crablet/lab/query/CrabletQueryApi.kt
+++ src/test/kotlin/crablet/lab/query/CrabletQueryApi.kt
@@ -1,19 +1,11 @@
-package crablet.query
+package crablet.lab.query
 
 import crablet.EventName
 import io.vertx.core.json.JsonObject
 import io.vertx.sqlclient.SqlConnection
 
-interface SubscriptionsRegistry {
-    suspend fun startSubscription(
-        name: String,
-        eventTypes: List<EventName>,
-        eventEffectFunction: (sqlConnection: SqlConnection, eventAsJson: JsonObject) -> JsonObject,
-        viewEffectFunction: (viewAsJson: JsonObject) -> Void,
-    )
-}
-
 data class SubscriptionConfig(
+    val name: String,
     val initialInterval: Long = DEFAULT_INITIAL_INTERVAL,
     val interval: Long = DEFAULT_INTERVAL,
     val maxNumberOfRows: Int = DEFAULT_NUMBER_ROWS,
@@ -22,9 +14,18 @@ data class SubscriptionConfig(
     val jitterFunction: () -> Int = { ((0..10).random() * 1000) },
 ) {
     companion object {
-        private const val DEFAULT_INITIAL_INTERVAL = 15_000L
+        private val DEFAULT_INITIAL_INTERVAL = ((1..10).random() * 1000L)
         private const val DEFAULT_INTERVAL = 5_000L
         private const val DEFAULT_NUMBER_ROWS = 250
         private const val DEFAULT_MAX_INTERVAL = 60_000L
     }
+}
+
+interface SubscriptionsRegistry {
+    suspend fun addSubscription(
+        config: SubscriptionConfig,
+        eventTypes: List<EventName>,
+        eventEffectFunction: (sqlConnection: SqlConnection, eventAsJson: JsonObject) -> JsonObject,
+        viewEffectFunction: ((viewAsJson: JsonObject) -> Void)? = null,
+    )
 }
