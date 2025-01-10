3a91a7fdf7909a6417d0f790b5ee01b380ece9c4
Rodolfo <rodolfo.depaula@uberall.com>
2025-01-10 15:09:53 -0300

    moved AccountsViewProjector to top level class

--- src/test/kotlin/crablet/query/AccountsViewProjectionTest.kt
+++ src/test/kotlin/crablet/query/AccountsViewProjectionTest.kt
@@ -193,42 +193,6 @@ class AccountsViewProjectionTest : AbstractCrabletTest() {
 
             val source = SubscriptionSource(name = "accounts-view", eventTypes = eventTypes)
 
-            class AccountsViewProjector : ViewProjector {
-                override fun project(sqlConnection: SqlConnection, eventAsJson: JsonObject): Future<Void> {
-                    val eventPayload = eventAsJson.getJsonObject("event_payload")
-
-                    return when (eventPayload.getString("type")) {
-
-                        "AccountOpened", "AmountDeposited" -> {
-                            val id = eventPayload.getInteger("id")
-                            val balance = eventPayload.getInteger("balance", 0)
-                            val upsertQuery =
-                                "INSERT INTO accounts_view(id, balance) VALUES($1, $2) ON CONFLICT (id) DO UPDATE SET balance = $2"
-                            sqlConnection.preparedQuery(upsertQuery)
-                                .execute(Tuple.of(id, balance))
-                                .mapEmpty()
-                        }
-
-                        "AmountTransferred" -> {
-                            val fromAcctId = eventPayload.getInteger("fromAcct")
-                            val fromAcctBalance = eventPayload.getInteger("fromAcctBalance")
-                            val toAcctId = eventPayload.getInteger("toAcct")
-                            val toAcctBalance = eventPayload.getInteger("toAcctBalance")
-
-                            val tuples = listOf(Tuple.of(fromAcctId, fromAcctBalance), Tuple.of(toAcctId, toAcctBalance))
-
-                            val upsertQuery =
-                                "UPDATE accounts_view SET balance = $2 WHERE id = $1"
-                            sqlConnection.preparedQuery(upsertQuery)
-                                .executeBatch(tuples)
-                                .mapEmpty()
-                        }
-
-                        else -> Future.succeededFuture()
-                    }
-                }
-            }
-
             val subscriptionConfig = SubscriptionConfig(
                 source = source,
                 viewProjector = AccountsViewProjector(),
---
+++ src/test/kotlin/crablet/query/AccountsViewProjector.kt
@@ -0,0 +1,50 @@
+package crablet.query
+
+import io.vertx.core.Future
+import io.vertx.core.json.JsonObject
+import io.vertx.sqlclient.SqlConnection
+import io.vertx.sqlclient.Tuple
+
+class AccountsViewProjector : ViewProjector {
+    override fun project(
+        sqlConnection: SqlConnection,
+        eventAsJson: JsonObject,
+    ): Future<Void> {
+        val eventPayload = eventAsJson.getJsonObject("event_payload")
+
+        return when (eventPayload.getString("type")) {
+            "AccountOpened", "AmountDeposited" -> {
+                val id = eventPayload.getInteger("id")
+                val balance = eventPayload.getInteger("balance", 0)
+                sqlConnection
+                    .preparedQuery(upsertQuery)
+                    .execute(Tuple.of(id, balance))
+                    .mapEmpty()
+            }
+
+            "AmountTransferred" -> {
+                val fromAcctId = eventPayload.getInteger("fromAcct")
+                val fromAcctBalance = eventPayload.getInteger("fromAcctBalance")
+                val toAcctId = eventPayload.getInteger("toAcct")
+                val toAcctBalance = eventPayload.getInteger("toAcctBalance")
+
+                val tuples = listOf(Tuple.of(fromAcctId, fromAcctBalance), Tuple.of(toAcctId, toAcctBalance))
+
+                sqlConnection
+                    .preparedQuery(updateQuery)
+                    .executeBatch(tuples)
+                    .mapEmpty()
+            }
+
+            else -> Future.succeededFuture()
+        }
+    }
+
+    companion object {
+        val upsertQuery =
+            "INSERT INTO accounts_view(id, balance) VALUES($1, $2) ON CONFLICT (id) DO UPDATE SET balance = $2"
+
+        val updateQuery =
+            "UPDATE accounts_view SET balance = $2 WHERE id = $1"
+    }
+}
