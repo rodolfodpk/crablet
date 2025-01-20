f68479f8d19c10762c5d0d142f3b9afae3e12401
Rodolfo <rodolfo.depaula@uberall.com>
2025-01-20 18:58:26 -0300

    improved get_hash function

--- src/main/resources/ddl/2-domain_ids_hash.sql
+++ src/main/resources/ddl/2-domain_ids_hash.sql
@@ -2,9 +2,17 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE OR REPLACE FUNCTION get_hash(_ids TEXT[]) RETURNS INT AS $$
 DECLARE
-    _temp text;
+    _temp TEXT;
 BEGIN
-    SELECT array_to_string(_ids, '') INTO _temp;
-    RETURN ('x' || SUBSTRING(ENCODE(DIGEST(_temp, 'sha256'), 'hex') FROM 1 FOR 8))::bit(32)::int;
+    -- Handle NULL or empty input
+    IF _ids IS NULL OR array_length(_ids, 1) IS NULL THEN
+        RETURN NULL;
+    END IF;
+
+    -- Sort array elements and concatenate with a separator to avoid collision
+    SELECT array_to_string(ARRAY(SELECT UNNEST(_ids) ORDER BY 1), ',') INTO _temp;
+
+    -- Hash and return an integer representation
+    RETURN ('x' || LEFT(ENCODE(DIGEST(_temp, 'sha256'), 'hex'), 8))::bit(32)::int;
 END;
 $$ LANGUAGE plpgsql;
